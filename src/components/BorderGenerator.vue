d7dd1a81dea10185ae2797af5c4fc850fbdf6e78
Max <scorpiomax90@gmail.com>
2023-09-14 00:01:54 +0300

    Deleted logic on slider, add slider css

--- src/components/BorderGenerator.vue
+++ src/components/BorderGenerator.vue
@@ -1,53 +1,17 @@
 <template>
   <section class="generator">
     <div class="generator__show"></div>
-    <span
-      class="generator__handler"
-      :style="{ left: sliderValue + '%' }"
-      @mousedown="startDrag"
-    ></span>
+    <input type="range" class="generator__handler" v-model="sliderValue">
+		<p>{{ sliderValue }}</p>
   </section>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-const isDragging = ref(false);
-const sliderValue = ref(50);
+const isDragging = ref(false)
+const sliderValue = ref(50)
 
-function startDrag(event) {
-  if (event.button !== 0) return;
-  const slider = event.target;
-  const rect = slider.getBoundingClientRect();
-  const minLeft = 0;
-  const maxLeft = 100 - (1.8 / rect.width) * 100; // 1.8 - ширина .generator__handler
-
-  isDragging.value = true;
-  let prevX = event.clientX; // Сохраняем начальное положение мыши
-
-  const handleDrag = (e) => {
-    if (isDragging.value) {
-      const deltaX = e.clientX - prevX;
-      const posX = (e.clientX - rect.left) / rect.width;
-      const newValue = Math.min(maxLeft, Math.max(minLeft, posX * 100));
-
-      // Добавляем ограничение для минимального перемещения
-      if (Math.abs(deltaX) >= 2) { // Измените это значение по вашему усмотрению
-        sliderValue.value = newValue;
-        prevX = e.clientX;
-      }
-    }
-  };
-
-  const stopDrag = () => {
-    isDragging.value = false;
-    window.removeEventListener('mousemove', handleDrag);
-    window.removeEventListener('mouseup', stopDrag);
-  };
-
-  window.addEventListener('mousemove', handleDrag);
-  window.addEventListener('mouseup', stopDrag);
-}
 </script>
 
 <style lang="scss" scoped>
@@ -72,15 +36,16 @@ function startDrag(event) {
 
   &__handler {
     position: absolute;
+		width: 100%;
     top: 0;
-    left: -0.9rem;
-    width: 1.8rem;
-    height: 1.8rem;
-    border-radius: 50%;
-    background: #000;
-    border: 3px solid white;
-    cursor: pointer;
-    transition: left 0.2s;
+    left: 0;
+    // width: 1.8rem;
+    // height: 1.8rem;
+    // border-radius: 50%;
+    // background: #000;
+    // border: 3px solid white;
+    // cursor: pointer;
+    // transition: left 0.2s;
   }
 }
 </style>
